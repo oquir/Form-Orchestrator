568c30eb1c0a6935373c5d161beeed0099c1214a
Oquir <tomasnz2003@gmail.com>
2026-08-02 14:09:04 -0500

    Mueve las clases de posicion del tooltip a constants global
    Las va a necesitar tambien el tooltip del simulador.

--- src/components/molecules/TooltipBubble/TooltipBubble.tsx
+++ src/components/molecules/TooltipBubble/TooltipBubble.tsx
@@ -1,15 +1,16 @@
-import { RichTextView } from "../../atoms/RichTextView/RichTextView";
 import {
   TOOLTIP_ANCHOR_CLASSES,
-  TOOLTIP_BUBBLE_CLASSES,
+  TOOLTIP_SURFACE_CLASSES,
   TOOLTIP_TEXT_CLASSES,
-} from "./TooltipBubble.constants";
+} from "../../../constants/fieldTooltip";
+import { RichTextView } from "../../atoms/RichTextView/RichTextView";
+import { TOOLTIP_BUBBLE_CLASSES } from "./TooltipBubble.constants";
 import type { TooltipBubbleProps } from "./TooltipBubble.types";
 
 export function TooltipBubble({ tooltip }: TooltipBubbleProps) {
   return (
     <div
-      className={`${TOOLTIP_BUBBLE_CLASSES} ${TOOLTIP_ANCHOR_CLASSES[tooltip.position]} ${
+      className={`${TOOLTIP_BUBBLE_CLASSES} ${TOOLTIP_ANCHOR_CLASSES[tooltip.position]} ${TOOLTIP_SURFACE_CLASSES} ${
         tooltip.customClasses ?? ""
       }`}
     >
--- src/constants/fieldTooltip.ts
+++ src/constants/fieldTooltip.ts
@@ -3,3 +3,15 @@ import type { TooltipPosition } from "../types/field";
 export const TOOLTIP_POSITIONS: TooltipPosition[] = ["top", "bottom", "left", "right"];
 
 export const DEFAULT_TOOLTIP_POSITION: TooltipPosition = "top";
+
+export const TOOLTIP_ANCHOR_CLASSES: Record<TooltipPosition, string> = {
+  top: "bottom-full left-1/2 mb-1.5 -translate-x-1/2",
+  bottom: "top-full left-1/2 mt-1.5 -translate-x-1/2",
+  left: "right-full top-1/2 mr-1.5 -translate-y-1/2",
+  right: "left-full top-1/2 ml-1.5 -translate-y-1/2",
+};
+
+export const TOOLTIP_SURFACE_CLASSES: string =
+  "w-max max-w-56 rounded-md bg-slate-800 px-2 py-1.5 shadow-lg dark:bg-neutral-700";
+
+export const TOOLTIP_TEXT_CLASSES: string = "text-[11px] text-white";
