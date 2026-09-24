e79eb6136823275807fbd12de4c348d6ed03d9f5
Tomas-Aguilar <tomasnz2003@gmail.com>
2026-09-24 17:25:09 -0500

    Describe el esquema Zod generado en su ⓘ

---
+++ src/components/molecules/GeneratedSchemaPreview/GeneratedSchemaPreview.constants.ts
@@ -0,0 +1,2 @@
+export const SCHEMA_DESCRIPTION: string =
+  "El esquema de validación que se exporta para este campo, armado con las reglas de arriba. Las validaciones condicionales viajan aparte, cada una con el suyo.";
--- src/components/molecules/GeneratedSchemaPreview/GeneratedSchemaPreview.tsx
+++ src/components/molecules/GeneratedSchemaPreview/GeneratedSchemaPreview.tsx
@@ -1,11 +1,13 @@
 import { CodeBlock } from "../../atoms/CodeBlock/CodeBlock";
 import { CopyIconButton } from "../../atoms/CopyIconButton/CopyIconButton";
 import { PanelSection } from "../PanelSection/PanelSection";
+import { SCHEMA_DESCRIPTION } from "./GeneratedSchemaPreview.constants";
 
 export function GeneratedSchemaPreview({ schema }: { schema: string }) {
   return (
     <PanelSection
       title="Esquema Zod generado"
+      description={SCHEMA_DESCRIPTION}
       aside={<CopyIconButton value={schema} title="Copiar el esquema" />}
     >
       <CodeBlock>{schema}</CodeBlock>
